77a4a3d55e977b0682c498263437f171d1b8c9b6
ipetak <ivan.petak1@gmail.com>
2020-08-13 00:12:36 +0200

    Fix Beta release tag

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: kekspay, woocommerce, gateway, payment
 Requires at least: 5.0
 Tested up to: 5.4.1
 Requires PHP: 7.2
-Stable tag: 1.0
+Stable tag: 0.9
 License: GPL v3 or later
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 
--- woocommerce-gateway-kekspay.php
+++ woocommerce-gateway-kekspay.php
@@ -3,7 +3,7 @@
  * Plugin Name:       KEKS Pay for WooCommerce
  * Plugin URI:        https://www.kekspay.hr/
  * Description:       KEKS Pay za WooCommerce.
- * Version:           1.0
+ * Version:           0.9
  * Requires at least: 5.0
  * Requires PHP:      7.2
  * Author:            Neuralab
@@ -129,7 +129,7 @@ if ( ! class_exists( 'WC_Kekspay' ) ) {
         define( 'KEKSPAY_PLUGIN_ID', 'erste-kekspay-woocommerce' );
       }
       if ( ! defined( 'KEKSPAY_PLUGIN_VERSION' ) ) {
-        define( 'KEKSPAY_PLUGIN_VERSION', '1.0' );
+        define( 'KEKSPAY_PLUGIN_VERSION', '0.9' );
       }
       if ( ! defined( 'KEKSPAY_DIR_PATH' ) ) {
         define( 'KEKSPAY_DIR_PATH', plugin_dir_path( __FILE__ ) );

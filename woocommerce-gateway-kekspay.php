f6c8d874f65e6589cec6027342606378fc1945a0
Ivan Petak <petak@neuralab.net>
2023-08-29 10:42:51 +0200

    Test HPOS and new versions of WC and WP

--- woocommerce-gateway-kekspay.php
+++ woocommerce-gateway-kekspay.php
@@ -3,7 +3,7 @@
  * Plugin Name:       KEKS Pay for WooCommerce
  * Plugin URI:        https://www.kekspay.hr/
  * Description:       Incredible fast and user friendly payment method created by Erste Bank Croatia.
- * Version:           1.0.12
+ * Version:           1.0.13
  * Requires at least: 5.0
  * Requires PHP:      7.2
  * Author:            Erste bank Croatia
@@ -14,7 +14,7 @@
  * Domain Path:       /languages
  *
  * WC requires at least: 3.3
- * WC tested up to: 7.2
+ * WC tested up to: 8.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -53,6 +53,19 @@ if ( ! kekspay_wc_active() ) {
   add_action( 'admin_notices', 'kekspay_admin_notice_missing_woocommerce' );
   return;
 }
+
+/**
+ * Declare Kekspay plugin compatible with HPOS.
+ *
+ * @return  void
+ */
+function kekspay_hpos_compatible() {
+  if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+    \Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+  }
+}
+add_action( 'before_woocommerce_init', 'kekspay_hpos_compatible' );
+
 
 if ( ! class_exists( 'WC_Kekspay' ) ) {
   /**
@@ -128,7 +141,7 @@ if ( ! class_exists( 'WC_Kekspay' ) ) {
         define( 'KEKSPAY_PLUGIN_ID', 'erste-kekspay-woocommerce' );
       }
       if ( ! defined( 'KEKSPAY_PLUGIN_VERSION' ) ) {
-        define( 'KEKSPAY_PLUGIN_VERSION', '1.0.12' );
+        define( 'KEKSPAY_PLUGIN_VERSION', '1.0.13' );
       }
       if ( ! defined( 'KEKSPAY_DIR_PATH' ) ) {
         define( 'KEKSPAY_DIR_PATH', plugin_dir_path( __FILE__ ) );

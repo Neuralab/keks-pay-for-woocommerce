421a11731c2fdedbb896145e7ebad252b369356b
Damir Babojelić <damir@neuralab.net>
2023-12-19 11:36:52 +0100

    Increase timeout for refund request

--- includes/core/class-kekspay-connector.php
+++ includes/core/class-kekspay-connector.php
@@ -38,7 +38,7 @@ if ( ! class_exists( 'Kekspay_Connector' ) ) {
           'Content-Type' => 'application/json',
         ),
         'method'  => 'POST',
-        'timeout' => 30,
+        'timeout' => 55,
         'body'    => $encoded_body,
         'cookies' => [],
       );
